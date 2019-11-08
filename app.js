c4bc022b3292f6f03c41d6327493f27d686cf5de
Dory <dory202gr@gmail.com>
2019-11-08 12:55:46 +0200

    level 2 - Encryption

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -23,6 +24,9 @@ const userSchema = mongoose.Schema({
     required: true
   }
 });
+
+var secret = "Thisisourlittlesecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
